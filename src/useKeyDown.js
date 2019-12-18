7b3981d48b8476a8c3eb6593fa591e96e775752f
杨魁 <c6swift@gmail.com>
2019-12-18 13:26:06 +0800

    fix: dont launch some event when type

--- src/useKeyDown.js
+++ src/useKeyDown.js
@@ -1,3 +1,5 @@
+// I don't know how to write the file to ts
+
 import { useRef } from 'react';
 
 function pipeWrapper(func) {
@@ -14,30 +16,47 @@ function pipeWrapper(func) {
   });
   return func;
 }
+
+const dontLaunchInType = (code: string) => /^[a-z\\,./;']$/.test(code);
+
+/**
+ *
+ * @param {string} code  bind keyboard
+ * @param {function} callback keyboard event callback
+ * @returns {{meta: boolean, shift: boolean, alt: boolean, ctrl: boolean, prevent: boolean, pipe: function}}
+ *
+ * a function that can use be compose another keydown event
+ */
 export default function useKeyDown(code, callback) {
   const isMeta = useRef(false);
   const isShift = useRef(false);
   const isAlt = useRef(false);
   const isCtrl = useRef(false);
   const isPrevent = useRef(false);
-  /**
-   *
-   * @param {React.KeyboardEvent} e
-   */
+
   function onKeyDown(e) {
+    const tagName = document.activeElement?.tagName?.toLowerCase() ?? '';
+    const canType = tagName === 'input' || tagName === 'textarea';
+    if (
+      canType &&
+      dontLaunchInType(code) &&
+      e.currentTarget !== document.activeElement
+    ) {
+      return;
+    }
     switch (e.key.toLowerCase()) {
       case code: {
-        // as same as isMeta && !e.metaKey || !isMeta && e.metaKey
-        if (isMeta.current ^ e.metaKey) {
+        // as same as isMeta.current && !e.metaKey || !isMeta.current && e.metaKey or e.metaKey ^ isMeta.current
+        if (isMeta.current !== e.metaKey) {
           return;
         }
-        if (isShift.current ^ e.shiftKey) {
+        if (isShift.current !== e.shiftKey) {
           return;
         }
-        if (isAlt.current ^ e.altKey) {
+        if (isAlt.current !== e.altKey) {
           return;
         }
-        if (isCtrl.current ^ e.ctrlKey) {
+        if (isCtrl.current !== e.ctrlKey) {
           return;
         }
         if (isPrevent.current) {
@@ -50,6 +69,7 @@ export default function useKeyDown(code, callback) {
         break;
     }
   }
+
   Object.defineProperties(onKeyDown, {
     prevent: {
       get() {
